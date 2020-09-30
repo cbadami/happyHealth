72350508f5999987e3b4bf4b742083dbc43c55f9
Harish Thadka <harish24680@gmail.com>
2020-09-30 03:25:50 -0500

    commented database connection

--- database.js
+++ database.js
@@ -1,24 +1,24 @@
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-  // socketPath: "exalted-legacy-290621:us-central1:happyhealth-test01",
-  host: "35.194.21.170",
-  user: "root",
-  password: "123456",
-  database: "happyhealth_MySQL"
-})
+// var mysql = require('mysql');
+// var conn = mysql.createConnection({
+//   // socketPath: "exalted-legacy-290621:us-central1:happyhealth-test01",
+//   host: "35.194.21.170",
+//   user: "root",
+//   password: "123456",
+//   database: "happyhealth_MySQL"
+// })
 
-//connect to database
-conn.connect(function(err) {
-  if(err){
-    return console.log("Error connecting to database: "+err.message);
-  }
-  console.log("connected to Google cloud MySQL server");
-});
-
-// Query 
-// conn.query("select emailid from User_Info where username = 'abcd'", function (err, result, fields) {
-//   if (err) throw err;
-//   console.log(result);
+// //connect to database
+// conn.connect(function(err) {
+//   if(err){
+//     return console.log("Error connecting to database: "+err.message);
+//   }
+//   console.log("connected to Google cloud MySQL server");
 // });
 
-module.exports = conn;
+// // Query 
+// // conn.query("select emailid from User_Info where username = 'abcd'", function (err, result, fields) {
+// //   if (err) throw err;
+// //   console.log(result);
+// // });
+
+// module.exports = conn;
