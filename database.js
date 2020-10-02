2f1b42d6cff40e97a462ebb702eb6fa1ac5fbe9e
Harish Thadka <harish24680@gmail.com>
2020-10-02 17:20:01 -0500

    added login check validation

--- database.js
+++ database.js
@@ -1,41 +1,40 @@
 
-// // Locally database connection:
-// var mysql = require('mysql');
-// var conn = mysql.createConnection({
-// //   socketPath: "/cloudsql/exalted-legacy-290621:us-central1:happyhealth-test01",
-//   host: "35.194.21.170",
-//   user: "master",
-//   password: "123456",
-//   database: "happyhealth_MySQL"
-// })
+// Locally database connection:
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: "35.194.21.170",
+  user: "master",
+  password: "123456",
+  database: "happyhealth_MySQL"
+})
 
-// //connect to database
-// conn.connect(function(err) {
-//   if(err){
-//     return console.log("Error connecting to database: "+err.message);
-//   }
-//   console.log("connected to Google cloud MySQL server");
-// });
-// module.exports = conn;
-
+//connect to database
+conn.connect(function(err) {
+  if(err){
+    return console.log("Error connecting to database: "+err.message);
+  }
+  console.log("connected to Google cloud MySQL server");
+});
+module.exports = conn;
 
 
 
-// Google cloud database connection deployment
-var mysql = require("mysql");
 
-const config = {
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE
-};
+// // Google cloud database connection deployment
+// var mysql = require("mysql");
 
-if (
-  process.env.INSTANCE_CONNECTION_NAME &&
-  process.env.NODE_ENV === "production"
-) {
-  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
-const connection = mysql.createConnection(config);
+// const config = {
+//   user: process.env.SQL_USER,
+//   password: process.env.SQL_PASSWORD,
+//   database: process.env.SQL_DATABASE
+// };
 
-module.exports = connection;
+// if (
+//   process.env.INSTANCE_CONNECTION_NAME &&
+//   process.env.NODE_ENV === "production"
+// ) {
+//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+// }
+// const connection = mysql.createConnection(config);
+
+// module.exports = connection;
