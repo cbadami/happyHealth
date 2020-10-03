186f0a1074518eb1bdcb9e6fb3e5c6cbd7f7523f
Harish Thadka <harish24680@gmail.com>
2020-10-02 22:06:36 -0500

    added login check validation

--- views/loginCheck.js
+++ views/loginCheck.js
@@ -1,6 +1,32 @@
 
-function loginCheckValidation() {
+  function loginCheckValidation() {
 
-
-}
-
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    console.log(x);
+  if (email == "") {
+  alert('please fill the Username');
+  }
+  //-  else {
+  //- if (re.test(x)) {
+  //- }
+  //- else {
+  //- alert("Please enter valid email address");
+  //- return false
+  //- }
+  //- }
+  //- if (document.getElementById('password').value == "") {
+  //- alert('please fill the password');
+  //- return false
+  //- }
+  //- else
+  //- {
+  //- if (reg.test(y)) {
+  //- }
+  //- else {
+  //- alert("password should contain atleast one number and one special character");
+  //- return false
+  //- }
+  //- }
+  //- alert("login successful");
+  }
