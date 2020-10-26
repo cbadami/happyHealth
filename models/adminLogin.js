006e7588758cb86e1f18c15f757c27120b052f6c
sowmya thogiti <sowmyathogiti27@gmail.com>
2020-10-25 19:31:40 -0500

    added attributes in admin login page model

--- models/adminLogin.js
+++ models/adminLogin.js
@@ -1,5 +1,5 @@
 /**
-*  admin page model
+*  admin login page model
 *  explains each attribute in a admin page.
 *
 * @author Sowmya Thogiti
@@ -9,31 +9,30 @@
 'use strict';
 var admin = {};
 module.exports = (sequelize, DataTypes) => {
-    admin= sequelize.define('admin', {
+    admin = sequelize.define('admin', {
         UserName: {
             type: DataTypes.UserName,
             primaryKey: true,
-             notEmpty: true
+            notEmpty: true,
+            validate: {
+                isUserName: true
+            }
         },
         Password: {
-            type: DataTypes.Password,           
+            type: DataTypes.Password,
+            allowNull: false,
+            notEmpty: true,
         },
         Forgot_Password: {
-            type: DataTypes.boolean,            
+            type: DataTypes.boolean,
         },
-        Social_Profiles: {
-                type: DataTypes.boolean,
-               
+
+        NotAdmin: {
+            type: DataTypes.boolean,
         },
-        Admin: {
-                type: DataTypes.boolean,
-        },
-        Signup: {
-                type: DataTypes.boolean,
-                },
-            }); 
-               admin.associate = function(models) {
-              // associations can be defined here
-                     };
-               return admin;
-               };
+    });
+    admin.associate = function (models) {
+        // associations can be defined here
+    };
+    return admin;
+};
