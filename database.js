1009885ad67c67914b3dda804ab7a2241e672f96
Harish Thadka <harish24680@gmail.com>
2020-10-31 03:06:17 -0500

    removed verification.pug file

--- database.js
+++ database.js
@@ -1,37 +1,37 @@
 
-// // Locally database connection:
-// var mysql = require('mysql');
-// var conn = mysql.createConnection({
-//   host: "35.194.21.170",
-//   user: "master",
-//   password: "123456",
-//   database: "happyhealth_MySQL",
-//   //port: 3000y
-// })
-// conn.connect(function(err) {
-//   if(err){
-//     return console.log("Error connecting to database: "+err.message);
-//   }
-//   console.log("connected to Google cloud MySQL server");
-// });
-// module.exports = conn;
+// Locally database connection:
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: "35.194.21.170",
+  user: "master",
+  password: "123456",
+  database: "happyhealth_MySQL",
+  //port: 3000y
+})
+conn.connect(function(err) {
+  if(err){
+    return console.log("Error connecting to database: "+err.message);
+  }
+  console.log("connected to Google cloud MySQL server");
+});
+module.exports = conn;
 
 
-// Google cloud database connection"
-var mysql = require("mysql");
+// // Google cloud database connection"
+// var mysql = require("mysql");
 
-const config = {
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE
-};
+// const config = {
+//   user: process.env.SQL_USER,
+//   password: process.env.SQL_PASSWORD,
+//   database: process.env.SQL_DATABASE
+// };
 
-if (
-  process.env.INSTANCE_CONNECTION_NAME &&
-  process.env.NODE_ENV === "production"
-) {
-  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
-const connection = mysql.createConnection(config);
+// if (
+//   process.env.INSTANCE_CONNECTION_NAME &&
+//   process.env.NODE_ENV === "production"
+// ) {
+//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+// }
+// const connection = mysql.createConnection(config);
 
-module.exports = connection;
+// module.exports = connection;
