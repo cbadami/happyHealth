cbe650a2ec22767a30708f2e4eaf8ccfb2700624
Harish Thadka <harish24680@gmail.com>
2020-11-03 16:01:57 -0600

    added erros in user step view

--- controllers/userHomeController.js
+++ controllers/userHomeController.js
@@ -7,6 +7,93 @@ exports.getUserHome = (req, res) => {
 }
  
 
+exports.getUserStep = (req,res) => {
+    res.render('userStep')
+}
+
+exports.postUserStep = (req,res) =>{
+    const { date, num_steps, goal } = req.body;
+    let errors = [];
+    let success_msg;
+    if (!date || !num_steps || !goal) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+    else {
+
+        res.redirect('/');
+        // if (name.length > 12) {
+        //     errors.push({ msg: 'Username must be below 12 characters' });
+        // }
+        // else if (name.length < 6) {
+        //     errors.push({ msg: 'Username must be atleast 6 characters' });
+        // }
+
+        // if (password.length > 15) {
+        //     errors.push({ msg: 'Password must be below 15 characters' });
+        // }
+        // else if (password.length < 8) {
+        //     errors.push({ msg: 'Password must be at least 8 characters' });
+        // }
+
+        // if (email.length > 30) {
+        //     errors.push({ msg: 'Email id must be below 30 characters' });
+        // }
+
+        // if (password != password2) {
+        //     errors.push({ msg: 'Passwords not matched' });
+        // }
+    }
+
+    // console.log(`before errors ${errors} length: ${errors.length}`);
+    // if (errors.length > 0) {
+    //     res.render('userSignup', {
+    //         errors,
+    //         name,
+    //         email,
+    //         password,
+    //         password2
+    //     });
+    // }
+    // else {
+    //     console.log(`errors in last query: ${errors[0]} length ${errors.length}`)
+    //     var queryString = `INSERT INTO  happyhealth_MySQL.USER values(
+    //     '${name}','${password}','No','No','No','Yes','${email}');`;
+    //     db.query(queryString, function (err, result) {
+    //         if (err) {
+    //             console.log(`${err}`)
+    //             let str = err.message
+    //             if(str.includes("UserName")){
+    //                 errors.push({ msg: 'Username already taken' });
+    //                 res.render('userSignup', {
+    //                     errors,
+    //                     name,
+    //                     email,
+    //                     password,
+    //                     password2
+    //                 }); 
+    //             }else if(str.includes("Email")){
+    //                 errors.push({ msg: 'Email id already registered' });
+    //                 res.render('userSignup', {
+    //                     errors,
+    //                     name,
+    //                     email,
+    //                     password,
+    //                     password2
+    //                 }); 
+    //             }
+    //         }else{
+    //         console.log("1 record inserted");
+    //         success_msg = 'Register sucessful';
+    //         req.session.success_msg = success_msg;
+    //         res.redirect('/')
+    //         }
+    //     });
+
+    // }
+}
+
+
+
 exports.getUserChallenges = (req, res) => {
     res.render('user_challenges')
 } 
@@ -20,9 +107,7 @@ exports.getUserSleep = (req,res) => {
     res.render('userSleep')
 }
 
-exports.getUserStep = (req,res) => {
-    res.render('userStep')
-}
 exports.getUserHydration = (req,res) => {
     res.render('userHydration')
 }
+
