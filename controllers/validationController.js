054de304ee892eea162049b5de65cb8691e53d79
Harish Thadka <harish24680@gmail.com>
2020-11-10 22:43:22 -0600

    working on mail implementation

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -1,15 +1,7 @@
-const nodemailer = require('nodemailer'); 
+const nodemailer = require('nodemailer');
 
-var code = Math.floor((Math.random() * 100000) + 1)
-var email = "<h1>Happy Health</h1> <p>Your otp is "+ code+"  </p>"
-  
-let mailTransporter = nodemailer.createTransport({ 
-    service: 'gmail', 
-    auth: { 
-        user: 'happyhealthgdp@gmail.com', 
-        pass: 'Happyhealth123'
-    } 
-}); 
+var generateCode;
+
 
 
 
@@ -17,6 +9,35 @@ exports.getValidation = (req, res) => {
   // const errors = req.errors;
   const userName = req.session.userName;
   console.log(`under get validation page ${userName}`);
+
+  generateCode = Math.floor(((Math.random()+1) * 100000));
+  console.log(`Generate code: ${generateCode}`)
+  var email = "<h1>Happy Health</h1> <p>Your otp is " + generateCode + "  </p>"
+
+  let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'happyhealthgdp@gmail.com',
+      pass: 'Happyhealth123'
+    }
+  });
+
+  let mailDetails = {
+    from: 'happyhealthgdp@gmail.com',
+    to: 'harishthadkaus@gmail.com',
+    subject: 'Happy Health forgot Password!',
+    // text: 'Node.js testing mail for GeeksforGeeks'
+    html: email
+  };
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log('Error Occurs ' + err);
+    } else {
+      console.log('Email sent successfully');
+    }
+  });
+
   res.render('validationPage');
 }
 
@@ -32,7 +53,7 @@ exports.postValidation = (req, res) => {
   if (!code) {
     errors.push({ msg: 'Enter verification code' });
   }
-  else if(code != '1111') {
+  else if (code != generateCode) {
     errors.push({ msg: 'Invalid verification code' });
   }
   if (errors.length > 0) {
@@ -40,7 +61,7 @@ exports.postValidation = (req, res) => {
       errors
     });
   }
-  else{
+  else {
     res.redirect('/resetPassword')
   }
 
--- mail.js
+++ mail.js
@@ -1,17 +1,2 @@
 
   
-let mailDetails = { 
-    from: 'happyhealthgdp@gmail.com', 
-    to: 'harishthadkaus@gmail.com', 
-    subject: 'Happy Health forgot Password!', 
-    // text: 'Node.js testing mail for GeeksforGeeks'
-    html: email
-}; 
-  
-mailTransporter.sendMail(mailDetails, function(err, data) { 
-    if(err) { 
-        console.log('Error Occurs '+err); 
-    } else { 
-        console.log('Email sent successfully'); 
-    } 
-}); 
