4fa9ab0a9396b03721cc01d7ec1d1b19ec421903
Harish Thadka <harish24680@gmail.com>
2020-11-24 23:42:58 -0600

    removed google cloud files

--- database.js
+++ database.js
@@ -17,23 +17,3 @@ db.connect(function (err) {
 });
 
 module.exports = db;
-
-
-// // Google cloud database connection"
-// var mysql = require("mysql");
-
-// const config = {
-//   user: process.env.SQL_USER,
-//   password: process.env.SQL_PASSWORD,
-//   database: process.env.SQL_DATABASE
-// };
-
-// if (
-//   process.env.INSTANCE_CONNECTION_NAME &&
-//   process.env.NODE_ENV === "production"
-// ) {
-//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-// }
-// const connection = mysql.createConnection(config);
-
-// module.exports = connection;
