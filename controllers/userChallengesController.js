1ac506343d67f008b8de86a95fd5b269377a028d
sindhu rani goli <saisindhurani@gmail.com>
2021-02-11 00:32:42 -0600

    created routes,controller for activeChallenges & availableChallenges

---
+++ controllers/userChallengesController.js
@@ -0,0 +1,39 @@
+const db = require('../database');
+
+
+exports.getActiveChallenges = (req, res) => {
+
+
+    res.render('userViews/activeChallenges', {layout: 'layouts/userLayout', title: 'Active Challenges' });
+    // const allGroupsQuery = `SELECT * FROM happyhealth.groupTbl`;
+
+    // db.query(allGroupsQuery, function (err, result) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         console.log(result, "------------db group result");
+    //         res.render('userViews/activeChallenges', {layout: 'layouts/userLayout', title: 'Active Challenges', result });
+    //         console.log("***************getGroup executed successfully******************");
+    //     }
+    // });
+
+};
+
+exports.getAvailableChallenges = (req, res) => {
+
+    res.send('test')
+
+    // const allGroupsQuery = `SELECT * FROM happyhealth.groupTbl`;
+
+    // db.query(allGroupsQuery, function (err, result) {
+    //     if (err) {
+    //         throw err;
+    //     } else {
+    //         console.log(result, "------------db group result");
+    //         res.render('adminViews/groupManagement', {layout: 'layouts/adminLayout', title: 'Group Management', result });
+    //         console.log("***************getGroup executed successfully******************");
+    //     }
+
+    // });
+
+};
