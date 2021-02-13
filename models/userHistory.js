a7d916e9c0c7f1d06bb2a44090a780abc9174f68
bharathkumar3355 <bharathkumar3355@gmail.com>
2021-02-12 23:07:30 -0600

    merge

---
+++ models/userHistory.js
@@ -0,0 +1,38 @@
+/**
+*  admin login page model
+*  explains each attribute in a admin page.
+*
+* @author Soujanya Janapatla
+* 
+*
+*/
+'use strict';
+var admin = {};
+module.exports = (sequelize, DataTypes) => {
+    admin = sequelize.define('admin', {
+        UserName: {
+            type: DataTypes.UserName,
+            primaryKey: true,
+            notEmpty: true,
+            validate: {
+                isUserName: true
+            }
+        },
+        Password: {
+            type: DataTypes.Password,
+            allowNull: false,
+            notEmpty: true,
+        },
+        Forgot_Password: {
+            type: DataTypes.boolean,
+        },
+
+        NotAdmin: {
+            type: DataTypes.boolean,
+        },
+    });
+    admin.associate = function (models) {
+        // associations can be defined here
+    };
+    return admin;
+};
