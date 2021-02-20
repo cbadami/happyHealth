8021fc907fe67001cb1d564955040b956d463966
sindhu rani goli <saisindhurani@gmail.com>
2021-02-20 17:44:45 -0600

    Available challenges controller is created
    challenges controller is created and need to display data in the views (Pending)

--- controllers/userChallengesController.js
+++ controllers/userChallengesController.js
@@ -4,7 +4,10 @@ const db = require('../database');
 exports.getActiveChallenges = (req, res) => {
 
 
-    res.render('userViews/activeChallenges', {layout: 'layouts/userLayout', title: 'Active Challenges' });
+    res.render('userViews/activeChallenges', {
+        layout: 'layouts/userLayout',
+        title: 'Active Challenges'
+    });
     // const allGroupsQuery = `SELECT * FROM happyhealth.groupTbl`;
 
     // db.query(allGroupsQuery, function (err, result) {
@@ -20,25 +23,31 @@ exports.getActiveChallenges = (req, res) => {
 };
 
 exports.getAvailableChallenges = (req, res) => {
+    const allAvailableChallenges = `Select challengeName, challengeDescription from happyhealth.challengetbl`;
+    
+    db.query(allAvailableChallenges, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(result, "------------db group result");
+            res.render('userViews/availableChallenges', {
+                layout: 'layouts/userLayout',
+                title: 'Available Challanges',
+                result
+            });
+            console.log("***************Available challanges executed successfully******************");
+        }
+    });
 
-    res.render('userViews/availableChallenges', {layout: 'layouts/userLayout', title: 'Available Challenges' });
-    // const allGroupsQuery = `SELECT * FROM happyhealth.groupTbl`;
-
-    // db.query(allGroupsQuery, function (err, result) {
-    //     if (err) {
-    //         throw err;
-    //     } else {
-    //         console.log(result, "------------db group result");
-    //         res.render('adminViews/groupManagement', {layout: 'layouts/adminLayout', title: 'Group Management', result });
-    //         console.log("***************getGroup executed successfully******************");
-    //     }
-
-    // });
-
+   
+     
 };
 
 exports.getCompletedChallenges = (req, res) => {
 
-    res.render('userViews/completedChallenges', {layout: 'layouts/userLayout', title: 'Completed Challenges' });
-    
+    res.render('userViews/completedChallenges', {
+        layout: 'layouts/userLayout',
+        title: 'Completed Challenges'
+    });
+
 };
