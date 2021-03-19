dbf8f12a5a6c931cd9fd460c11f38f5e6e89d362
annie0sc <annie0sc@gmail.com>
2021-03-19 00:10:15 -0500

    Adding get and post controllers for announcements.

--- controllers/user/userNotificationController.js
+++ controllers/user/userNotificationController.js
@@ -1,10 +1,64 @@
 const db = require('../../database');
 
-
 exports.getNotifications = (req, res) => {
-    res.render('userViews/notifications', {
-        layout: 'layouts/userLayout',
-        title: 'User Management'
-
+    let messageId = req.session.messageId;
+    const nnQuery = `Select messageId, title, message, userId from happyhealth.announcementsTbl where messageId = ${messageId};`;
+    db.query(nnQuery, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result, '-------displaying user notifictions-----');
+            const {
+                title,
+                message,
+                userId
+            } = result[0];
+            res.render('userViews/notifications', {
+                layout: 'layouts/userLayout',
+                title: 'Announcements',
+                messageId,
+                title,
+                message,
+                userId
+            });
+        }
     });
+
 };
+
+
+exports.postNotifications = (req, res) => {
+    let userId = req.session.userId;
+    const {
+        messageId,
+        title,
+        message
+    } = req.body;
+    console.log("-------post user Notification controller");
+    let errors = [];
+    if (!messageId || !title || !message) {
+        errors.push('Please enter all fields');
+        console.log(errors, "----------------errros");
+        res.render('userViews/notifications', {
+            layout: 'layouts/userLayout',
+            title: 'Announcements'
+        });
+    }
+    var ppQuery = `Delete FROM happyhealth.announcementstbl WHERE messageId = '${messageId}';`;
+    db.query(ppQuery, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('userViews/notifications');
+        }
+    });
+
+};
+
+// exports.getNotifications = (req, res) => {
+//     res.render('userViews/notifications', {
+//         layout: 'layouts/userLayout',
+//         title: 'User Management'
+
+//     });
+// };
