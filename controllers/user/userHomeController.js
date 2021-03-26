7f25edfd2d2a8741515f947d600d0e79d9cd6ae1
chanduhvg <chanduhvg@gmail.com>
2021-03-25 23:35:55 -0500

    updated userhome controller

--- controllers/user/userHomeController.js
+++ controllers/user/userHomeController.js
@@ -413,41 +413,32 @@ exports.postUserPhysicalActivity = (req, res) => {
 	});
 };
 
-exports.resetUserMetrics = (req, res) => {
-	console.log('running a task every minute');
+exports.resetUserMetrics = async (req, res) => {
+	console.log('running cron job at every day 12:am');
 
-//Schedule tasks to be run on the server.
-cron.schedule('0 12 * * *', async function () {
-	try {
-		// console.log('running a task every minute');
-		const usersQuery = 'SELECT userId FROM usertbl';
-		await db.query(usersQuery, (err, result) => {
-			if (err) {
-				console.log(err, '------error while users');
-			} else {
-				let usersCount = result.length;
-				let users = result;
-				let values = '';
+	const usersQuery = 'SELECT userId FROM usertbl';
+	await db.query(usersQuery, (err, result) => {
+		if (err) {
+			console.log(err, '------error while users');
+		} else {
+			let usersCount = result.length;
+			let users = result;
+			let values = '';
 
-				for (let i = 0; i < usersCount; i++) {
-					values += `(${users[i].userId},"${currentDate}",0,0,0,0,0,0,0,0,0,0,0,0,0,0 ),`;
-				}
-
-				console.log(values.slice(0, -1), '========> values to insert');
-
-				const newValuesQuery = `INSERT INTO happyhealth.usermetricstbl (userId, date, stepCount, stepGoal, sleepHours, sleepGoal, meTime, meTimeGoal, water, waterGoal, fruits, fruitGoal, veggies, veggieGoal, physicalActivityMinutes, physicalActivityGoal) values ${values}`;
-				db.query(newValuesQuery, (err, result) => {
-					if (err) {
-						console.log(err, '============> error while reseting values');
-					} else {
-						console.log(result, '===========> insert new values');
-					}
-				});
+			for (let i = 0; i < usersCount; i++) {
+				values += `(${users[i].userId},"${currentDate}",0,0,0,0,0,0,0,0,0,0,0,0,0,0 ),`;
 			}
-		});
-	} catch (err) {
-		console.log(err, '============> error while runnning cron jobs');
-	}
-});
 
+			console.log(values.slice(0, -1), '========> values to insert');
+
+			const newValuesQuery = `INSERT INTO happyhealth.usermetricstbl (userId, date, stepCount, stepGoal, sleepHours, sleepGoal, meTime, meTimeGoal, water, waterGoal, fruits, fruitGoal, veggies, veggieGoal, physicalActivityMinutes, physicalActivityGoal) values ${values}`;
+			db.query(newValuesQuery, (err, result) => {
+				if (err) {
+					console.log(err, '============> error while reseting values');
+				} else {
+					console.log(result, '===========> insert new values');
+				}
+			});
+		}
+	});
 };
