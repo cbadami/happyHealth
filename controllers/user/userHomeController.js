29fbbaba57eb14ee8277c5e351cd229818d7d9fc
srkvodnala <srkrao547@gmail.com>
2021-03-25 23:47:11 -0500

    updated time

--- controllers/user/userHomeController.js
+++ controllers/user/userHomeController.js
@@ -414,31 +414,39 @@ exports.postUserPhysicalActivity = (req, res) => {
 };
 
 exports.resetUserMetrics = async (req, res) => {
-	console.log('running cron job at every day 12:am');
+	cron.schedule('0 0 * * *', async () => {
+		try{
+			console.log('running cron job at every day 12:am');
 
-	const usersQuery = 'SELECT userId FROM usertbl';
-	await db.query(usersQuery, (err, result) => {
-		if (err) {
-			console.log(err, '------error while users');
-		} else {
-			let usersCount = result.length;
-			let users = result;
-			let values = '';
-
-			for (let i = 0; i < usersCount; i++) {
-				values += `(${users[i].userId},"${currentDate}",0,0,0,0,0,0,0,0,0,0,0,0,0,0 ),`;
-			}
-
-			console.log(values.slice(0, -1), '========> values to insert');
-
-			const newValuesQuery = `INSERT INTO happyhealth.usermetricstbl (userId, date, stepCount, stepGoal, sleepHours, sleepGoal, meTime, meTimeGoal, water, waterGoal, fruits, fruitGoal, veggies, veggieGoal, physicalActivityMinutes, physicalActivityGoal) values ${values}`;
-			db.query(newValuesQuery, (err, result) => {
+			const usersQuery = 'SELECT userId FROM usertbl';
+			await db.query(usersQuery, (err, result) => {
 				if (err) {
-					console.log(err, '============> error while reseting values');
+					console.log(err, '------error while users');
 				} else {
-					console.log(result, '===========> insert new values');
+					let usersCount = result.length;
+					let users = result;
+					let values = '';
+		
+					for (let i = 0; i < usersCount; i++) {
+						values += `(${users[i].userId},"${currentDate}",0,0,0,0,0,0,0,0,0,0,0,0,0,0 ),`;
+					}
+		
+					console.log(values.slice(0, -1), '========> values to insert');
+		
+					const newValuesQuery = `INSERT INTO happyhealth.usermetricstbl (userId, date, stepCount, stepGoal, sleepHours, sleepGoal, meTime, meTimeGoal, water, waterGoal, fruits, fruitGoal, veggies, veggieGoal, physicalActivityMinutes, physicalActivityGoal) values ${values}`;
+					db.query(newValuesQuery, (err, result) => {
+						if (err) {
+							console.log(err, '============> error while reseting values');
+						} else {
+							console.log(result, '===========> insert new values');
+						}
+					});
 				}
-			});
+			});		
+		}catch(err){
+			console.log(err, "=======> error while running cron job")
 		}
 	});
+
+
 };
