257eccecf47d7fc043882f943d41495495f88c28
alekhyajaddu <alekhyajaddu@gmail.com>
2021-04-21 01:10:15 -0500

    updated challenge members and notification page

--- routes/user.js
+++ routes/user.js
@@ -82,6 +82,7 @@ router.post('/userPhysicalActivity', userHomeController.postUserPhysicalActivity
   */
  
 router.get('/notifications', userNotificationController.getNotifications);
+router.post('/viewNotification/:messageId',userNotificationController.viewNotification )
 // router.post('/notifications', userNotificationController.postNotifications);
 router.post('/dismissAnnouncement/:messageId', userNotificationController.dismissAnnouncement)
 
